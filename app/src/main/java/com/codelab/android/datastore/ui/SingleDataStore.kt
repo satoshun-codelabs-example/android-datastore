0cb09a53eeaa1f9dfa0df1cee3d235ce771162d5
SatoShun <shun.sato1@gmail.com>
2020-09-08 01:51:18 +0000

    add migration

--- app/src/main/java/com/codelab/android/datastore/ui/SingleDataStore.kt
+++ app/src/main/java/com/codelab/android/datastore/ui/SingleDataStore.kt
@@ -1,12 +1,9 @@
 package com.codelab.android.datastore.ui
 
 import android.content.Context
+import androidx.datastore.DataMigration
 import androidx.datastore.DataStore
-import androidx.datastore.preferences.Preferences
-import androidx.datastore.preferences.createDataStore
-import androidx.datastore.preferences.edit
-import androidx.datastore.preferences.emptyPreferences
-import androidx.datastore.preferences.preferencesKey
+import androidx.datastore.preferences.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
@@ -14,18 +11,23 @@ import kotlinx.coroutines.flow.map
 import java.io.IOException
 
 inline fun <reified T : Any> Context.singleDataStore(
-  name: String
+  name: String,
+  migrationKey: String? = null
 ): SingleDataStore<T> {
   val key = preferencesKey<T>(name)
-  return SingleDataStoreImpl(this, key)
+  val migrations =
+    if (migrationKey != null) listOf(SharedPreferencesMigration(this, migrationKey))
+    else emptyList()
+  return SingleDataStoreImpl(this, key, migrations)
 }
 
 class SingleDataStoreImpl<T : Any>(
   context: Context,
-  private val key: Preferences.Key<T>
+  private val key: Preferences.Key<T>,
+  migrations: List<DataMigration<Preferences>>
 ) : SingleDataStore<T> {
   private val dataStore: DataStore<Preferences> =
-    context.createDataStore(name = key.name)
+    context.createDataStore(name = key.name, migrations = migrations)
 
   override val data: Flow<T>
     get() = dataStore.data
